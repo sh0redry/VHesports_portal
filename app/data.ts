eb42e606a60c8f911786aee7a773c3d58087d987
sh0redry <sh0redry@connect.hku.hk>
2026-08-24 11:11:59 +0800

    0.2.0 game tag updated

--- app/data.ts
+++ app/data.ts
@@ -3,17 +3,19 @@ export const asset = (name: string) => `/images/source/${encodeURIComponent(name
 
 export const games = {
   mobile: [
-    ["Mobile Legends", "mobile game3.jpg"], ["Honor of Kings", "mobile game4.jpg"], ["PUBG Mobile", "mobile game5.jpg"], ["Call of Duty: Mobile", "mobile game6.jpg"],
+    ["Mobile Legends", "Mobile LOL.png"],
+    ["Honor of Kings", "mobile honor of king.png"],
   ],
   pc: [
-    ["VALORANT", "PC game1.jpg"], ["League of Legends", "pc game2.png"], ["EA SPORTS FC", "pc game3.png"], ["Counter-Strike 2", "pc game4.png"], ["Minecraft", "pc game5.png"],
+    ["VALORANT", "Valorant.png"],
+    ["Counter-Strike 2", "CS2.png"],
   ],
 };
 
 export const committee = [
-  { name: "Song Xinhang", roles: ["Leader", "Vice Leader - Mobile Games"], photo: "Song xinhang photo.jpg" },
-  { name: "Zhao Dingjia", preferred: "Justin", roles: ["Vice Leader", "Leader - PC Games"], photo: "Zhao Dingjia photo.jpg" },
-  { name: "Xiao Yadan", roles: ["Leader - Mobile Games"], photo: "Xiao Yadan photo.jpg" },
-  { name: "Kyler Zhao", roles: ["Vice Leader - PC Games"], photo: "Kyler Zhao photo.jpg" },
-  { name: "Gary Yang", roles: ["Logistics"], photo: "Gary Yang photo.jpg" },
+  { name: "Song Xinhang", roles: ["Leader", "Vice Leader - Mobile Games"], photo: "Song xinhang photo.jpg", telegram: "@Song_Xinhang" },
+  { name: "Zhao Dingjia", preferred: "Justin", roles: ["Vice Leader", "Leader - PC Games"], photo: "Zhao Dingjia photo.jpg", telegram: "@Justin_Zha0" },
+  { name: "Xiao Yadan", roles: ["Leader - Mobile Games"], photo: "Xiao Yadan photo.jpg", telegram: "@Angelina_XYD" },
+  { name: "Kyler Zhao", roles: ["Vice Leader - PC Games"], photo: "Kyler Zhao photo.jpg", telegram: "@KylerZhao" },
+  { name: "Gary Yang", roles: ["Logistics"], photo: "Gary Yang photo.jpg", telegram: "@Gy_yang" },
 ];
